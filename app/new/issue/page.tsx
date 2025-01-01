9b58937cebc7776bde806008a59e9139cf12a73e
nishchal <nishchalbhardwaj2004@gmail.com>
2025-01-01 13:35:22 +0530

    adding form handler

--- app/new/issue/page.tsx
+++ app/new/issue/page.tsx
@@ -1,19 +1,36 @@
 "use client"
 import { Button,  TextField } from '@radix-ui/themes'
-import React from 'react'
+import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
+import axios from "axios"
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
 
 
 const NewIssuePage = () => {
+   const router= useRouter()
+    const [title,setTitle]=useState("")
+    const [description,setDescription]=useState("")
   return (
     <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Write the title"  />
+        <TextField.Root onChange={(e)=>{
+            setTitle(e.target.value)
+        }}  placeholder="Write the title"  />
         
 
-        <SimpleMDE  placeholder='Add the decription'/>
+        <SimpleMDE onChange={(value)=>{
+            setDescription(value)
+        }}  placeholder='Add the decription'/>
 
-        <Button>Submit new Issue</Button>
+        <Button onClick={async()=>{
+            
+            await axios.post("/api/issue",{
+                title,description
+            })
+            router.push("/issues")
+
+            
+        }}>Submit new Issue</Button>
 
     </div>
   )
