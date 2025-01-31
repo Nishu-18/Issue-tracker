7d95eb02248133a3b6b88958ea26d9e425bda544
nishchal <nishchalbhardwaj2004@gmail.com>
2025-02-01 00:22:28 +0530

    adding suspense in issueFilter

--- app/issues/list/IssueAction.tsx
+++ app/issues/list/IssueAction.tsx
@@ -1,13 +1,15 @@
 import { Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import StatusFilter from './StatusFilter'
 
 const IssueAction = () => {
   return (
     
         <Flex  justify={"between"}>
+          <Suspense fallback={<div>Loading...</div>}>
           <StatusFilter/>
+          </Suspense>
               <Link href={"/new/issue"}>
               <Button>New Issue</Button>
               </Link>
